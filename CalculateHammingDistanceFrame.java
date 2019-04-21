1b5db069ff893fd164fc7f470054a6d598748922
Christian McBane <christian.j.mcbane-1@ou.edu>
2019-04-21 11:14:15 -0500

    add labels to calculateHD

--- CalculateHammingDistanceFrame.java
+++ CalculateHammingDistanceFrame.java
@@ -1,23 +1,52 @@
 import java.awt.GridLayout;
+import java.awt.Insets;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class CalculateHammingDistanceFrame extends JFrame 
 {
-	JPanel left = new JPanel(new GridLayout(4, 1));
-	JPanel right = new JPanel(new GridLayout(4, 1));
+	JPanel main = new JPanel(new GridLayout(5, 1)) {public Insets getInsets() {return new Insets(15, 15, 15, 15);}};
+	JPanel distance0 = new JPanel(new GridLayout(1, 2));
+	JPanel distance1 = new JPanel(new GridLayout(1, 2));
+	JPanel distance2 = new JPanel(new GridLayout(1, 2));
+	JPanel distance3 = new JPanel(new GridLayout(1, 2));
+	JPanel distance4 = new JPanel(new GridLayout(1, 2));
+	
+	JLabel distance0Label = new JLabel("Distance 0");
+	JLabel distance1Label = new JLabel("Distance 1");
+	JLabel distance2Label = new JLabel("Distance 2");
+	JLabel distance3Label = new JLabel("Distance 3");
+	JLabel distance4Label = new JLabel("Distance 4");
 	
 	public CalculateHammingDistanceFrame()
 	{
 		super("Hamming Distances");
 		this.setSize(300, 400);
-		this.setLayout(new GridLayout(1, 2));
+		this.setLayout(new GridLayout(1, 1));
 		
+		addLabelsAndText();
 		
-		this.add(left);
-		this.add(right);
+		this.add(main);
 		
 		this.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		this.setVisible(true);
+	}
+
+	private void addLabelsAndText() 
+	{
+		main.add(distance0);
+		main.add(distance1);
+		main.add(distance2);
+		main.add(distance3);
+		main.add(distance4);
+		
+		distance0.add(distance0Label);
+		distance1.add(distance1Label);
+		distance2.add(distance2Label);
+		distance3.add(distance3Label);
+		distance4.add(distance4Label);
+		
 	}
 }
