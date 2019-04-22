57a525cd251cd1031835585059b48f2694924004
Christian McBane <christian.j.mcbane-1@ou.edu>
2019-04-21 20:24:26 -0500

    add comments to MesoStations

--- MesoStations.java
+++ MesoStations.java
@@ -2,12 +2,29 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-
+/**
+ * Class to get the Station IDs from a file
+ * 
+ * @author CJGam
+ * @version 04-21-2019 
+ */
 public class MesoStations 
 {
+	/**
+	 * List of all station IDs
+	 */
 	private ArrayList<String> stations;
+	
+	/**
+	 * File to be read From
+	 */
 	private String fileName;
 	
+	/**
+	 * Constructor for creating a list of all stations
+	 * 
+	 * @param fileName File to read for Station IDs
+	 */
 	public  MesoStations(String fileName)
 	{
 		this.fileName = fileName;
@@ -20,13 +37,20 @@ public class MesoStations
 		}
 	}
 
+	/**
+	 * Reads a file containing station IDs and puts them in an ArrayList
+	 * 
+	 * @return ArrayList with all station IDs
+	 * @throws IOException
+	 */
 	private ArrayList<String> getStationsFromFile() throws IOException 
 	{
 		// oversize to allow for more stations to be added before resizing, original file has 120 stations
 		ArrayList<String> stations = new ArrayList<String>(140);
 		
 		BufferedReader br = new BufferedReader(new FileReader(fileName));
-				
+		
+		// String will be read in with the while loop header
 		String currLine = "";
 		while ((currLine = br.readLine()) != null)
 		{
@@ -36,11 +60,21 @@ public class MesoStations
 		return stations;
 	}
 
+	/**
+	 * Gets the ArrayList with all stations in the file
+	 * 
+	 * @return ArrayList with all stations
+	 */
 	public ArrayList<String> getStations() 
 	{
 		return new ArrayList<String>(stations);
 	}
 	
+	/**
+	 * Add new station
+	 * 
+	 * @param station Station to be added to the stationList
+	 */
 	public void addStation(String station)
 	{
 		stations.add(station);
