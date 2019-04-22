26f86871d62912745035a79bd31222c7f4a1ab7c
Christian McBane <christian.j.mcbane-1@ou.edu>
2019-04-21 20:14:34 -0500

    add comments to HammingDist

--- HammingDist.java
+++ HammingDist.java
@@ -1,46 +1,96 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-
+/**
+ * Class to find hamming distance based on input mesoStation and input Station name
+ * 
+ * @author CJGam
+ * @version 04-21-2019
+ */
 public class HammingDist 
 {
+	/**
+	 * Station ID to compare everything to
+	 */
 	private String StID;
+	
+	/**
+	 * Contains all station IDs in the file
+	 */
 	private MesoStations meso;
 	
+	/**
+	 * all stations with distance 1
+	 */
 	private ArrayList<String> distance1;
+	/**
+	 * all stations with distance 2
+	 */
 	private ArrayList<String> distance2;
+	/**
+	 * all stations with distance 3
+	 */
 	private ArrayList<String> distance3;
+	/**
+	 * all stations with distance 4
+	 */
 	private ArrayList<String> distance4;
 	
+	/**
+	 * number of stations at each distance, indexed by distance (index 1 = distance 1)
+	 */
 	private int[] numDist;
 	
+	/**
+	 * Constructor to find Hamming Distances
+	 * 
+	 * @param meso MesoStations to be compared to
+	 * @param StID Station ID to get hamming distances for
+	 */
 	public HammingDist(MesoStations meso, String StID)
 	{
+		// initialize distance ArrayLists
 		distance1 = new ArrayList<String>();
 		distance2 = new ArrayList<String>();
 		distance3 = new ArrayList<String>();
 		distance4 = new ArrayList<String>();
 		
+		// initialize array containing Hamming Distances
 		numDist = new int[5];
 		
 		this.meso = meso;
 		this.StID = StID;
 		
+		// automatically calculate hamming distances
 		findHammingDistances(StID);
 	}
 
+	/**
+	 * Finds all hamming distances for the input Station
+	 * 
+	 * @param station Station to find Hamming distances for
+	 */
 	private void findHammingDistances(String station) 
 	{
 		ArrayList<String> mesoStations = meso.getStations();
 		
+		// get station at each index in Mesostations
 		for (int i = 0; i < mesoStations.size(); ++i)
 		{
+			// gets the hamming distance of each index of currStation
 			String currStation = mesoStations.get(i);
 			int currDist = calculateHammingDistance(StID, currStation);
 			
+			// sort the current station into respective array
 			addHammingArray(currDist, currStation);
 		}
 	}
 
+	/**
+	 * Sorts the stations to each ArrayList based on Hamming Distance
+	 * 
+	 * @param currDist Hamming distance of currStation
+	 * @param currStation Station to add to ArrayList corresponding to currDist
+	 */
 	private void addHammingArray(int currDist, String currStation) 
 	{
 		switch (currDist)
@@ -68,18 +118,27 @@ public class HammingDist
 				break;
 		}
 		
+		// increment the number of stations with that distance, distance between 0 and 4
 		if (currDist >= 0 && currDist <= 4)
 		{
 			++this.numDist[currDist];
 		}
 	}
 
+	/**
+	 * Calculates the hamming distance of two strings
+	 * 
+	 * @param station Initial Station to be compared to
+	 * @param currStation Current station to compare to Initial Station
+	 * @return Hamming distance of both input strings
+	 */
 	public static int calculateHammingDistance(String station, String currStation)
 	{
 		int distance = 0;
 		
 		for (int i = 0; i < station.length() && i < currStation.length(); ++i)
 		{
+			// compare characters at each index, if different increment Hamming distance
 			if (station.charAt(i) != currStation.charAt(i))
 			{
 				++distance;
@@ -89,11 +148,23 @@ public class HammingDist
 		return distance;
 	}
 
+	/**
+	 * Gets the calculated number of stations at each hamming distance
+	 * 
+	 * @return Integer array that contains the number of stations at each hamming distance
+	 */
 	public int[] getNumDist() 
 	{
 		return Arrays.copyOf(numDist, numDist.length);
 	}
 
+	/**
+	 * gets the ArrayList<String> with the stations with the same hamming distance from the object staion as
+	 * the value
+	 * 
+	 * @param value Distance of arrayList to find
+	 * @return ArrayList<String> with corresponding distance of value
+	 */
 	public ArrayList<String> getDistanceArray(int value) 
 	{
 		ArrayList<String> distanceNow = new ArrayList<String>();
@@ -119,6 +190,5 @@ public class HammingDist
 		
 		return distanceNow;
 	}
-	
-	
+		
 }
